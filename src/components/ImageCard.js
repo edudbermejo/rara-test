2431915aea33cf24a4cba7b11e35b3a0e0a81183
Eduardo David González <edudbermejo@gmail.com>
2019-11-19 16:46:18 +0000

    feat: date sohwing fixed

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,6 +5,9 @@ import './ImageCard.css'
 
 function ImageCard({imageDetails}) {
 
+  const date = new Date(imageDetails.dateTaken)
+  const dateToShow = date.toDateString()
+
   return  (
     <div className="image-card">
       <img src={`https://farm${imageDetails.farm}.staticflickr.com/${imageDetails.server}/${imageDetails.id}_${imageDetails.secret}.jpg`} alt="thumbnail" />
@@ -12,7 +15,7 @@ function ImageCard({imageDetails}) {
         <div className="image-information">
           <InformationBlock label="Title" info={imageDetails.title} />
           <InformationBlock label="Owner name" info={imageDetails.ownerName} />
-          <InformationBlock label="Date" info={imageDetails.dateTaken} />
+          <InformationBlock label="Date" info={dateToShow} />
         </div>
         <a className="large-image-anchor" href={imageDetails.originalUrl}><button>View large image</button></a>
       </div>
