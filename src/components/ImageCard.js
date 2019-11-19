d1f2ee0d7cd3b2a4c019fdd967aa3ce27812be50
Eduardo David González <edudbermejo@gmail.com>
2019-11-19 13:47:43 +0000

    styles: desktop styles deployed

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,13 +7,15 @@ function ImageCard({imageDetails}) {
 
   return  (
     <div className="image-card">
-      <img src={`https://farm${imageDetails.farm}.staticflickr.com/${imageDetails.server}/${imageDetails.id}_${imageDetails.secret}.jpg`} alt="thumbnail"></img>
-      <div className="image-information">
-        <InformationBlock label="Title" info={imageDetails.title} />
-        <InformationBlock label="Owner name" info={imageDetails.ownerName} />
-        <InformationBlock label="Date" info={imageDetails.dateTaken} />
+      <img src={`https://farm${imageDetails.farm}.staticflickr.com/${imageDetails.server}/${imageDetails.id}_${imageDetails.secret}.jpg`} alt="thumbnail" />
+      <div className="image-textual-container">
+        <div className="image-information">
+          <InformationBlock label="Title" info={imageDetails.title} />
+          <InformationBlock label="Owner name" info={imageDetails.ownerName} />
+          <InformationBlock label="Date" info={imageDetails.dateTaken} />
+        </div>
+        <a className="large-image-anchor" href={imageDetails.originalUrl}><button>View large image</button></a>
       </div>
-      <a href={imageDetails.originalUrl}><button>View large</button></a>
     </div>
   )
 }
